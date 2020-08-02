72274bfb8448d09ec6fb4fbc02f3a16ea93a1782
delyle <delylepolet@gmail.com>
2020-08-02 15:50:17 -0600

    Modified validation functions and added one to test pq complementarity

--- validation/Check_psidot_ldot.m
+++ validation/Check_psidot_ldot.m
@@ -4,9 +4,16 @@ aux = out(end).result.setup.auxdata;
 d = aux.d;
 D = aux.D;
 r = aux.r;
+lmax = aux.lmax;
+i = 1;
 
-[psidot,ldot] = psidot_ldot_fun(X(:,1:6),r,D);
-[psi,l] = psi_l_fun(X(:,1:6),r,D);
+P = [0,D,d];
+
+F = X(:,7:9);
+Tau = X(:,10:12);
+
+[psidot,ldot] = psidot_ldot_fun(X(:,1:6),r,P(i));
+[psi,l] = psi_l_fun(X(:,1:6),r,d);
 
 psidot_num = [diff(psi)./diff(t);NaN];
 ldot_num = [diff(l)./diff(t);NaN];
@@ -30,3 +37,10 @@ plot(t,psidot_num,'o')
 
 yyaxis right
 plot(t,psi)
+
+
+figure
+plot(t,lmax - l)
+hold on
+plot(t,F(:,i)./max(F(:,i)))
+plot(t,Tau(:,i))
